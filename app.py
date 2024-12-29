f7c1fdb3ecf3b3f92f277613bccb2064119e8176
Pavan.Kumar <pavanln049@gmail.com>
2024-12-29 01:16:32 +0100

    chatbot that shows all the aksed questiins and answer

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 from openai import OpenAI
 from config import secreat_key
@@ -13,7 +14,36 @@ st.title("HR. Chatbot")
 
 client = OpenAI(api_key = api_key)
 
-
 # Setting up the model fro the session state.
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-4o"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+if prompt := st.chat_input("your answer."):
+    st.session_state.messages.append({"role":"user", "content":prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            # model cheks the model
+            model = st.session_state["openai_model"],
+            # message parameter that takes entire chat history to improve the assistance response.
+            messages = [
+                {
+                    "role": m["role"], "content": m["content"]
+
+                }
+                for m in st.session_state.messages
+            ],
+            stream = True, # to receive the response as stream.
+
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role":"assistant", "content": response})
+
 
 
